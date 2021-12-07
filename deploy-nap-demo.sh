f6fb830c93cf2dae32258b2e32ee008902b1b8c7
Daniel Schuler <daniel_schuler@me.com>
2021-12-07 15:46:17 +0100

    Update deploy-nap-demo.sh

--- deploy-nap-demo.sh
+++ deploy-nap-demo.sh
@@ -1,45 +1,21 @@
 ## DEPLY GLOBAL filse 
 
-sleep .5
 kubectl apply -f 1_basicdeployment/ns-and-sa.yaml
 sleep .5
-kubectl apply -f 1_basicdeployment/rbac.yaml
+kubectl apply -f 1_basicdeployment/nginx/
 sleep .5
-kubectl apply -f 1_basicdeployment/ap-rbac.yaml
+kubectl apply -f 1_basicdeployment/nginx/rbac
+sleep .5
+kubectl apply -f 1_basicdeployment/nginx/crds
 sleep .5
 kubectl apply -f 1_basicdeployment/default-server-secret.yaml
 sleep .5
-kubectl apply -f 1_basicdeployment/nginx-config.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/vs-definition.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/vsr-definition.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/ts-definition.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/policy-definition.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/gc-definition.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/global-configuration.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/ap-logconf-definition.yaml
-sleep .5
-kubectl apply -f 1_basicdeployment/ap-policy-definition.yaml
+kubectl apply -f 1_basicdeployment/rbac.yaml
 sleep .5
 
 ## DEPLOY Ingress NGINX PLUS & Service for Ingress & dashboard for nginx
 
 kubectl apply -f 2_nginx_ingress/
-sleep .5
-
-## DEPLOY arcadia & dvwa & Hackazon
-
-kubectl apply -f 3_arcadia_app/
-sleep .5
-kubectl apply -f 4_dvwa/
-sleep .5
-kubectl apply -f 5_hackazon/
 sleep .5
 
 ## DEPLOY arcadia & dvwa & Hackazon
